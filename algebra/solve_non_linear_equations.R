21bd3e3a2e29cd003379c236c309fe857f712fcb
Till Bieg <till.bieg@gmx.net>
2020-02-06 09:53:08 +0100

    Fixed length of vector containing function values

--- algebra/solve_non_linear_equations.R
+++ algebra/solve_non_linear_equations.R
@@ -3,12 +3,11 @@ library(tidyverse)
 library(plotly)
 
 # Explore behaviour of function via simulation
-func_values <- vector(mode = "numeric")
+func_values <- vector(mode = "numeric", 20000)
 for (i in 1:20000) {
   x <- i - 10000
   
-  func_values <- c(func_values,
-                   x^6 - 2*x^4 + 4*x^3 - 3*x^2 - 4*x + 4)
+  func_values[i] <- (x^6 - 2*x^4 + 4*x^3 - 3*x^2 - 4*x + 4)
   
   # Could be defined outside of the for loop as well
   names(func_values)[i] <- x
@@ -18,16 +17,19 @@ for (i in 1:20000) {
 # Note that the plot is a bit misleading since many values
 # on the y-axis are huge! However, the plot gives an idea about the
 # behavior of the function
-func_plot <-tibble(x_value = as.numeric(names(func_values)),
-                   func_value = func_values) %>% 
+func_plot <- tibble(x_value = as.numeric(names(func_values)),
+                    func_value = func_values) %>% 
   ggplot(aes(x = x_value, func_value)) +
   geom_line() +
   theme_bw()
 
 ggplotly(func_plot)
 
-# Check which values are exactly 0
-roots <- func_values[func_values == 0]
+# Check for which x-values the function equals 0
+func_values[func_values == 0]
+
+# Save those values in a vector
+roots <- as.numeric(names(func_values[func_values == 0]))
 
 # Using nleqslv::nleqslv() to solve the function
 # Define the function you want to solve
@@ -38,4 +40,4 @@ fn <- function(x) {
 # The first parameter is an initial guess for the root of the function
 # A root is a value for which a given function equals zero
 # Function values from the simuation that equal 0 can be used as initial guesses
-map(c(-2, -1, 1), ~ nleqslv(.x, fn))
+map(roots, ~ nleqslv(.x, fn))
