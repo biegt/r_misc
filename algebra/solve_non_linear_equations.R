fe9e093222d3caedd6bd185f199b0c3a318b3e55
Till Bieg <till.bieg@gmx.net>
2020-02-06 09:33:31 +0100

    Add simulation to find initial guesses for roots of the function

--- algebra/solve_non_linear_equations.R
+++ algebra/solve_non_linear_equations.R
@@ -2,17 +2,7 @@ library(nleqslv)
 library(tidyverse)
 library(plotly)
 
-# Define the function you want to solve
-fn <- function(x) {
-  return(x^6 - 2*x^4 + 4*x^3 - 3*x^2 - 4*x + 4)
-}
-
-# Use nleqslv::nleqslv() to solve the function
-# The first parameter is an initial guess for the root of the function
-# A root is a value for which a given function equals zero
-nleqslv(1, fn)
-
-# Different a approach: Explore behaviour of function via simulation
+# Explore behaviour of function via simulation
 func_values <- vector(mode = "numeric")
 for (i in 1:20000) {
   x <- i - 10000
@@ -24,22 +14,28 @@ for (i in 1:20000) {
   names(func_values)[i] <- x
 }
 
-# Check which values are exactly 0
-func_values[func_values == 0]
-
 # Create interactive plot of function
-# Note that the plot is a little bit misleading since many values
+# Note that the plot is a bit misleading since many values
 # on the y-axis are huge! However, the plot gives an idea about the
 # behavior of the function
 func_plot <-tibble(x_value = as.numeric(names(func_values)),
                    func_value = func_values) %>% 
-              ggplot(aes(x = x_value, func_value)) +
-              geom_line() +
-              theme_bw()
+  ggplot(aes(x = x_value, func_value)) +
+  geom_line() +
+  theme_bw()
 
 ggplotly(func_plot)
 
-# Limitation: Only integers are considered
-# for examplematlab::linspace() could be used
-# to create linearly spaced vectors to iterate over
+# Check which values are exactly 0
+roots <- func_values[func_values == 0]
 
+# Using nleqslv::nleqslv() to solve the function
+# Define the function you want to solve
+fn <- function(x) {
+  return(x^6 - 2*x^4 + 4*x^3 - 3*x^2 - 4*x + 4)
+}
+
+# The first parameter is an initial guess for the root of the function
+# A root is a value for which a given function equals zero
+# Function values from the simuation that equal 0 can be used as initial guesses
+map(c(-2, -1, 1), ~ nleqslv(.x, fn))
